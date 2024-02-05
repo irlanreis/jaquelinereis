862f6cc2e6da22986e5a13103ed669470b36ff24
irlanreis <xuiexpress@gmail.com>
2024-02-05 18:21:39 -0400

    Feat: Insigths vercel

--- site-jaqueline/src/App.tsx
+++ site-jaqueline/src/App.tsx
@@ -1,9 +1,11 @@
 import Home from "./pages/Home";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 function App() {
   return (
     <div>
       <Home />
+      <SpeedInsights />
     </div>
   );
 }
