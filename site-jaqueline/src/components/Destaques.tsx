c7f31c0ede658c02da9149e31aa0701b9eeb5410
irlanreis <xuiexpress@gmail.com>
2024-02-05 23:26:59 -0400

    Feat: Implementa Serviços na home

--- site-jaqueline/src/components/Destaques.tsx
+++ site-jaqueline/src/components/Destaques.tsx
@@ -61,18 +61,75 @@ function Destaques() {
   }
 
   return (
-    <div className="w-full bg-black flex relative justify-center">
-      <h2 className="text-white p-2 text-6xl font-extralight bg-black pb-10 absolute">Destaques</h2>
-      <Slider className="bg-black pb-8 2xl:w-full 2xl:mt-44 ml-2" {...settings}>
-        {images.map((img, i) => (
+    <div className="bg-black">
+      <div className="w-full bg-black flex relative justify-center">
+        <h2 className="text-white p-2 text-6xl font-extralight bg-black pb-10 absolute">Destaques</h2>
+        <Slider className="bg-black pb-8 2xl:w-full 2xl:mt-44 ml-2" {...settings}>
+          {images.map((img, i) => (
             <img
               key={i}
               className="sm:w-80 2xl:m-8 2xl:mb-0 2xl:w-40 pl-4"
               src={img}
               alt={`design ${i + 1}`}
             />
-        ))}
-      </Slider>
+          ))}
+        </Slider>
+      </div>
+
+      <h2 className="text-white p-2 text-6xl font-extralight bg-black pb-10 m-10 w-1/5 items-center ml-auto mr-auto text-center">Serviços</h2>
+
+      <div className="flex bg-black justify-center w-full space-x-6">
+        <div className="w-80 h-96 rounded overflow-hidden shadow-lg ml-2 bg-yellow-900 text-center transition-transform transform hover:scale-105 hover:shadow-amber-600">
+          <div className="px-6 py-4 text-center space-y-20">
+            <div className="font-bold text-4xl mb-2 text-amber-500 mt-6 text-center">Gestão de Redes Sociais</div>
+            <p className="text-gray-50 text-lg font-light">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione, exercitationem, nam sequi possimus inventore.
+            </p>
+          </div>
+          <span className="p-2 bg-amber-950 rounded text-amber-500 hover:cursor-pointer hover:bg-amber-800">Valores ao Final</span>
+        </div>
+
+        <div className="w-80 h-96 rounded overflow-hidden shadow-lg ml-2 bg-yellow-900 text-center transition-transform transform hover:scale-105 hover:shadow-amber-600">
+          <div className="px-6 py-4 text-center space-y-20">
+            <div className="font-bold text-4xl mb-2 text-amber-500 mt-6 text-center">Criação de Desing</div>
+            <p className="text-gray-50 text-lg font-light">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione, exercitationem, nam sequi possimus inventore.
+            </p>
+          </div>
+          <span className="p-2 bg-amber-950 rounded text-amber-500 hover:cursor-pointer hover:bg-amber-800">Valores ao Final</span>
+        </div>
+
+        <div className="w-80 h-96 rounded overflow-hidden shadow-lg ml-2 bg-yellow-900 text-center transition-transform transform hover:scale-105 hover:shadow-amber-600">
+          <div className="px-6 py-4 text-center space-y-20">
+            <div className="font-bold text-4xl mb-2 text-amber-500 mt-6 text-center">Identidade Visual</div>
+            <p className="text-gray-50 text-lg font-light">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione, exercitationem, nam sequi possimus inventore.
+            </p>
+          </div>
+          <span className="p-2 bg-amber-950 rounded text-amber-500 hover:cursor-pointer hover:bg-amber-800">Valores ao Final</span>
+        </div>
+
+        <div className="w-80 h-96 rounded overflow-hidden shadow-lg ml-2 bg-yellow-900 text-center transition-transform transform hover:scale-105 hover:shadow-amber-600">
+          <div className="px-6 py-4 text-center space-y-20">
+            <div className="font-bold text-4xl mb-2 text-amber-500 mt-6 text-center">Análise de Perfil e Consultoria</div>
+            <p className="text-gray-50 text-lg font-light">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione, exercitationem, nam sequi possimus inventore.
+            </p>
+          </div>
+          <span className="p-2 bg-amber-950 rounded text-amber-500 hover:cursor-pointer hover:bg-amber-800">Valores ao Final</span>
+        </div>
+
+        <div className="w-80 h-96 rounded overflow-hidden shadow-lg ml-2 bg-yellow-900 text-center transition-transform transform hover:scale-105 hover:shadow-amber-600">
+          <div className="px-6 py-4 text-center space-y-20">
+            <div className="font-bold text-4xl mb-2 text-amber-500 mt-6 text-center">Criação de Landing Pages</div>
+            <p className="text-gray-50 text-lg font-light">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ratione, exercitationem, nam sequi possimus inventore.
+            </p>
+          </div>
+          <span className="p-2 bg-amber-950 rounded text-amber-500 hover:cursor-pointer hover:bg-amber-800">Valores ao Final</span>
+        </div>
+
+      </div>
     </div>
   )
 }
